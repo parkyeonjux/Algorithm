f5f76b01fd5ac85c3f6fef388f6639ad11bc5496
yeonju.park <126536408+parkyeonjux@users.noreply.github.com>
2023-11-19 20:09:16 +0900

    백준 - 2178 미로탐색.py

---
+++ 백준/2178 미로탐색.py
@@ -0,0 +1,21 @@
+def bfs(si, sj, ei, ej):
+  q = []
+  v = [[0]*M for _ in range(N) ]
+  q.append((si,sj))
+  v[si][sj] = 1 
+
+  while q:
+    ci, cj = q.pop(0)
+    #정답 처리가 필요한 경우 이자리에서 
+    if (ci,cj) == (ei,ej):
+      return v[ei][ej]
+
+  #4방향, 범위내, 조건에 맞으면: arr==1, v==0)
+  for di,dj in ((-1,0), (1,0), (0,-1), (0,1)):
+    ni, nj = ci+di, cj+dj
+    if 0 <=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
+      v[ni][nj] = v[ci][cj]+1
+N,M = mapp(unt, input().split())
+arr = [list(map(int, input())) for _ in range(N) ]
+ans = bfs(0,0,N-1, M-1)
+print(ans)
